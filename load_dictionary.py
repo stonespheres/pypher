a974d3ab35ac88f492768bae84713a633ba037f3
Steve Kollios <stephenkollios@gmail.com>
2021-03-24 10:15:32 +0800

    Refined dictionary

--- load_dictionary.py
+++ load_dictionary.py
@@ -1,22 +1,16 @@
-"""Load a text file as a list.
-Arguments:
--text file name
-Exceptions:
--IOError if filename not found.
-Returns:
--A list of all words in text file in lower case.
-Requires-import sys
-"""
+#Loads a text file and outputs a list
+
+#Requires
 import sys
 
-def load(file):
-    """Open a text file & turn contents into a list of lowercase strings."""
+def loadFile(file):
+    #Open a file and return lowercase list of strings
     try:
         with open(file) as in_file:
+            #Remove whitespace
             loaded_txt = in_file.read().strip().split('\n')
             loaded_txt = [x.lower() for x in loaded_txt]
             return loaded_txt
-    except IOError as e:
-        print("{}\nError opening {}. Terminating program.".format(e, file),
-              file=sys.stderr)
+    except IOError as error:
+        print("{}Error opening {}. Terminating program.".format(error, file), file=sys.stderr)
         sys.exit(1)
