fa412fff1508b2cce4eddd6e70ef7166c4670097
Steve Kollios <stephenkollios@gmail.com>
2021-03-27 21:36:46 +0800

    Improved the function of cypher.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Pypher
 Steganography is the practice of concealing messages so well, their very existence isn't even suspected.
 
-This program works using the *null cipher*
-It works by embedding a message in an image by subtly altering colour components. This makes the message practically invisible to the human eye.
-
+This program works using the *null cipher*. 
--- cypher.py
+++ cypher.py
@@ -7,7 +7,7 @@ import string
 import load_dictionary
 
 # Message should use no punctuation or numbers
-user_message = input("Input a message to cipher. The message should not contain punctuation or numnbers\n")
+user_message = input("Input a message to cipher. The message should not contain punctuation or numbers\n")
 user_message.translate(string.punctuation)
 message = ''
 for char in user_message:
@@ -18,15 +18,18 @@ message = "".join(message.split())
 
 # open dictionary file
 word_list = load_dictionary.loadFile('dictionary.txt')
-
+i = 0
 # build vocabulary word list with hidden message
 vocab_list = []
 for letter in message:
     size = randint(6, 10)
     for word in word_list:
-        if len(word) == size and word[2].lower() == letter.lower()\
+        if len(word) == size and word[i].lower() == letter.lower()\
         and word not in vocab_list:
             vocab_list.append(word)
+            i+=1
+            if i > 4:
+                i = 0
             break
         
 if len(vocab_list) < len(message):
@@ -34,3 +37,4 @@ if len(vocab_list) < len(message):
 else:        
     print("Vocabulary words for Unit 1: \n", *vocab_list, sep="\n")        
   
+
