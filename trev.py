10d70f58d1429bba5b43337bd3e41cb3ea6e5e1f
Steve Kollios <stephenkollios@gmail.com>
2021-03-27 11:52:57 +0800

    Added pseudo code for trevanian cypher

---
+++ trev.py
@@ -0,0 +1,17 @@
+# Load a text file, remove whitespace
+# Check user input for where to check for punctuation mark
+# Loop through user value from 1 to the specified value
+    # Start empty string to hold the translation
+    # Initiate a counter
+    # Start the found marker and set to false
+    # Loop through the characters
+        # If a character is a punctuation mark
+            # Counter = 0
+            # First-found = True
+        # Otherwise, if first-found is True
+            # Counter++
+        # If counter = lookahead value
+            # Add character to the translation string
+        # Display the translation to the user.
+
+        
